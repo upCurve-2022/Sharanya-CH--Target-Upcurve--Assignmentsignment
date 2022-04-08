c8bf0b1a798f158953c035f0c0fd0cc76e051778
SharanyaCH <73130507+SharanyaCH@users.noreply.github.com>
2022-04-08 11:23:00 +0530

    Delete Answer5.java

--- Answer5.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Answer5 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        System.out.println(s.length());
-    }
-}
